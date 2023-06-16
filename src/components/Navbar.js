26fb0d3b99d6848c498268418fd72e40b6b611c7
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-17 01:34:53 +0430

    Add nav styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import logo from '../img/Group 7.png'
 import victors from '../img/Group 639.png'
 import zoom from '../img/search-normal.png'
 import navItems from '../img/Group 640.png'
+import userImg from '../img/image 1.png'
 
 export default function Navbar() {
   return (
@@ -16,6 +17,10 @@ export default function Navbar() {
       <input type='text' placeholder={`search something...`} className='search'/>
       <img src={navItems} className='navItems'/>
       <h2 className='user'>Anima Agrawal</h2>
+      <h2 className='location'>U.P, India</h2>
+      <img src={userImg} className='user-img'/>
+      <img src={victor} className='victor'/>
+
     </div>
   )
 }
