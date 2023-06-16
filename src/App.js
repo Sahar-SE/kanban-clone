99d0c2b0cc22bcb0cd00d76f903a77bc6874f406
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-16 23:31:34 +0430

    Add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div>
-    <h1 className="text-3xl font-bold underline">Hello</h1>
-    <h1 className="text-3xl font-bold text-slate-300">
-         Hello world!
-       </h1>
-       </div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+import logo from '../img/Group 7.png'
+
+export default function Navbar() {
+  return (
+    <div className='flex'>
+      <img src={logo}/>
+      <h1 className='title'>Project M.</h1>   
+    </div>
+  )
+}
