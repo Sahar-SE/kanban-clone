55e056174849dcfb8f2f58f74aee02787ece93a9
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-16 23:43:31 +0430

    Add nav styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../App.css'
 import logo from '../img/Group 7.png'
+import victors from '../img/Group 639.png'
 
 export default function Navbar() {
   return (
     <div className='flex'>
       <img src={logo} className='logo'/>
-      <h1 className='title'>Project M.</h1>   
+      <h1 className='title'>Project M.</h1>
+      <img src={victors}  className='victors'/>  
     </div>
   )
 }
