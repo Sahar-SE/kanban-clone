df1be0cc6b964ed204c6fd8bb5f1c35abbab12b3
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-17 00:39:27 +0430

    Add nav styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../App.css'
 import logo from '../img/Group 7.png'
 import victors from '../img/Group 639.png'
+import zoom from '../img/search-normal.png'
 
 export default function Navbar() {
   return (
@@ -9,7 +10,9 @@ export default function Navbar() {
       <img src={logo} className='logo'/>
       <h1 className='title'>Project M.</h1>
       <img src={victors}  className='victors'/> 
-      <div className='nav-line'></div> 
+      <div className='nav-line'></div>
+      <div className='side-line'></div>
+      <input type='text' placeholder={`search something...`} className='search'/>
     </div>
   )
 }
