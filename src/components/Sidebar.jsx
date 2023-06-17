945c949c93c8bebff821a54c37adf37a7216f810
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-17 16:01:20 +0430

    Add side bar styles

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Sidebar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><img src={home}/>Home</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
