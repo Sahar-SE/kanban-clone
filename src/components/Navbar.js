8c3b84298b206af5b32a2619db51ac203dacd0e8
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-17 12:39:33 +0430

    Add nav styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ import victors from '../img/Group 639.png'
 import zoom from '../img/search-normal.png'
 import navItems from '../img/Group 640.png'
 import userImg from '../img/image 1.png'
+import victor from '../img/Vector.png'
 
 export default function Navbar() {
   return (
@@ -15,6 +16,7 @@ export default function Navbar() {
       <div className='nav-line'></div>
       <div className='side-line'></div>
       <input type='text' placeholder={`search something...`} className='search'/>
+      <img src={zoom} className='zoom' />
       <img src={navItems} className='navItems'/>
       <h2 className='user'>Anima Agrawal</h2>
       <h2 className='location'>U.P, India</h2>
