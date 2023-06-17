8ffb312ed10fbccd25d567cf28da4c635ba77fbe
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-17 16:31:13 +0430

    Add side bar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import home from '../img/home.png'
 import message from '../img/message.png'
+import task from '../img/message.png'
+import member from '../img/profile-2user.png'
+import setting from '../img/setting-2.png'
 
 export default function Sidebar() {
   return (
@@ -8,7 +11,7 @@ export default function Sidebar() {
       <nav>
         <table className='sideItems'>
           <tr>
-            <td className='p-2'><img src={home} className=''/></td>
+            <td className='p-2'><img src={home}/></td>
             <td className='p-2'>Home</td>
           </tr>
 
@@ -19,17 +22,17 @@ export default function Sidebar() {
 
           <tr>
             <td className='p-2'><img src={task} /></td>
-            <td className='p-2'>Messages</td>
+            <td className='p-2'>Tasks</td>
           </tr>
 
           <tr>
             <td className='p-2'><img src={member} /></td>
-            <td className='p-2'>Messages</td>
+            <td className='p-2'>Member</td>
           </tr>
 
           <tr>
             <td className='p-2'><img src={setting} /></td>
-            <td className='p-2'>Messages</td>
+            <td className='p-2'>Settings</td>
           </tr>
 
         </table>
