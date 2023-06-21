3cd33b4f3812a029a48e7a25f91368c0813a782e
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2023-06-21 21:07:10 +0430

    Add Main Contents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Main from "./components/Main";
-// import Dnd from "./components/Dnd";
+
 function App() {
   return (
     <div>
       <Navbar />
       <Sidebar />
-      {/* <Dnd />    */}
       <Main />
     </div>
   );
